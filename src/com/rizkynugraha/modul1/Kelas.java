2c87a3ec7cd381141a87797a861325c57e2f3509
Rizky Nugraha Herliyanto <56204095+lordacil@users.noreply.github.com>
2020-10-10 12:39:26 +0700

    Update Kelas.java

--- src/com/rizkynugraha/modul1/Kelas.java
+++ src/com/rizkynugraha/modul1/Kelas.java
@@ -2,4 +2,16 @@ package com.rizkynugraha.modul1;
 
 public class Kelas {
 
+    private static String nama = "Paimon";
+
+    private static int SetMinute(){
+        int minute = 10;
+        return minute;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Nama\t\t:" + nama);
+        System.out.println("Set Minute\t:" + SetMinute());
+    }
+  
 }
